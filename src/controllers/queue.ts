c747d16233f31dfed90eca4f0f50cb916766910d
Ashu <Matheusn.Biolowons@gmail.com>
2024-10-31 12:29:09 -0300

    🧪 Try it twice

--- src/controllers/queue.ts
+++ src/controllers/queue.ts
@@ -1,11 +1,16 @@
-import QueueBull, { QueueOptions } from 'bull'
+import QueueBull, { QueueOptions, Queue as QueueType } from 'bull'
 
-export class Queue extends QueueBull {
+export class Queue<T> extends QueueBull<T> {
+  private queues: Record<string, QueueType> = {}
+
   constructor(queueName: string, opts?: QueueOptions) {
     super(queueName, {
       ...opts,
+      defaultJobOptions: {
+        attempts: 2,
+      },
       limiter: {
-        max: 50,
+        max: 4,
         duration: 10000,
       },
       redis: {
@@ -13,5 +18,7 @@ export class Queue extends QueueBull {
         port: 6379
       }
     })
+
+    this.queues[this.name] = Object.assign(this, { queues: null })
   }
 }

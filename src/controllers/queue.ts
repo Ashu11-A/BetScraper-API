0950fb2e0118e74b5e375d0a694240869551b970
Ashu <Matheusn.Biolowons@gmail.com>
2024-11-05 22:07:10 -0300

    ⏳ Implementing delay in a row

--- src/controllers/queue.ts
+++ src/controllers/queue.ts
@@ -8,6 +8,7 @@ export class Queue<T> extends QueueBull<T> {
       ...opts,
       defaultJobOptions: {
         attempts: 2,
+        delay: 10000,
       },
       redis: {
         host: process.env['REDIS_HOST'],

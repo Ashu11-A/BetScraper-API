5637b25373fc0e92507698db3952604d6445eed2
Ashu <Matheusn.Biolowons@gmail.com>
2024-12-18 16:39:18 -0300

    ✨ Lint

--- src/app.ts
+++ src/app.ts
@@ -9,26 +9,26 @@ import { Fastify } from './controllers/fastify.js'
 import { Router } from './controllers/router.js'
 import Database from './database/dataSource.js'
 import { BetQueue } from './queues/BetQueue.js'
-import { runQueue } from 'scripts/runAll.js'
 
-// await BetQueue.removeAllRepeatable()
 execSync('bun run migration:run || true', { stdio: 'inherit' })
 
 const fastify = new Fastify({ port: 3000, host: '0.0.0.0' })
 await Database.initialize()
+// await Database.dropDatabase()
+
 await registerCrons()
 await registerCompliances()
 await registerBets()
+
 BetQueue.initialize()
-// await Database.dropDatabase()
+// await BetQueue.removeAllRepeatable()
 
 // await runOnlyUnregistered()
-await runQueue({ mode: 'primary' })
+// await runQueue({ mode: 'primary' })
 
 fastify.init()
 await Router.register()
 fastify.listen()
 
 await BetQueue.checkAllCrons()
-
 // console.log((await BetQueue.queue.getJobs([])))
